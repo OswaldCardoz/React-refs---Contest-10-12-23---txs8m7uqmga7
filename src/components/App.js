d09cd830ae59de9de84ca2d3f3ba255fe440105a
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-10 21:44:27 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,44 @@
 import React,{useState,useEffect,useRef} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const emailref=useRef();
+  const passworref=useRef();
+  const [ans,setAns]=useState(false);
+  const em=useRef("");
+  const ps=useRef("");
+
 
 //code here 
-const [email,setEmail]=useState('');
-const [password,setPassword]=useState('');
-const emailRef=useRef(null);
-const passwordRef=useRef(null);
-
-useEffect(()=>{
-  emailRef.current.focus();
-},[]);
-
-const handleEmail=(e)=>{
-setEmail(e.target.value)
+const handle=()=>{
+  if(email === ""){
+    emailref.current.focus();
+    setAns(false);
+  }else if(password === ""){
+    passworref.current.focus();
+    setAns(false);
+  }else{
+    setAns(true);
+    em.current=email;
+    ps.current=password;
+    setEmail("");
+    setPassword("");
+  }
 }
-const handlePass=(e)=>{
-setPassword(e.target.value)
-}
-const handleSubmit=()=>{
-  const emailText=document.getElementById('emailText');
- const passText=document.getElementById('passwordText');
- const emailIP=emailRef.current;
- const passwordIP=passwordRef.current;
-
-if(emailIP && passwordIP && emailText&&passText)
-{  
-  emailText.innerText=`Your Email : ${email}`;
-  passText.innerText=`Your Password : ${password}`;
-  if(!email.trim()){
-    emailIP.focus();
-  }else if(!password.trim()){
-    passwordIP.focus();
-  }}
-};
-
 
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={email} ref={emailRef} onChange={handleEmail}/><br/>
+      <input id="inputEmail" type="text" value={email} ref={emailref}
+       onChange={(e)=>setEmail(e.target.value)}/><br/>
       Password
-      <input id="inputPassword" type="text" value={password} ref={passwordRef} onChange={handlePass}/><br/>
-      <button id="submitButton" onClick={handleSubmit}>Submit</button><br/>
-      <p id="emailText">Your Email : {email}</p>
-      <p id ="passwordText">Your Password : {password}</p>
+      <input id="inputPassword" type="text" value={password} ref={passworref} 
+      onChange={(e)=>setPassword(e.target.value)}/><br/>
+
+      <button id="submitButton" onClick={handle}>Submit</button><br/>
+      <p id="emailText">Your Email : {ans &&  em.current }</p>
+      <p id ="passwordText">Your Password : {ans && ps.current }</p>
       
     </div>
   )
